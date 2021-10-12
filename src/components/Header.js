f84eb100a86dc27516bd2c1fa0b272300b07b875
Mark Reed <mreed@Admins-MacBook-Pro.local>
2021-10-12 16:58:33 -0500

    hamburger animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import './Header.css'
 import MobileMenuContext from './MobileMenuContext'
@@ -20,12 +20,17 @@ const LI = styled.li`
 const HamburgerMenu = styled.div`
     display:inline
 `
-
 export default function Header() {
     const [ mobileMenuOpen, setMobileMenuOpen ] = useContext(MobileMenuContext)
-    const openMobileMenu = (e) => {
+    const toggleMobileMenu = () => {
         setMobileMenuOpen(!mobileMenuOpen)
+        setAnimated1(animated1 === "animated1"? "no-animate1" : "animated1")
+        setAnimated2(animated2 === "animated2"? "no-animate2" : "animated2")
+        setAnimated3(animated3 === "animated3"? "no-animate3" : "animated3")
     }
+    const [ animated1, setAnimated1 ] = useState("no-animate1")
+    const [ animated2, setAnimated2 ] = useState("no-animate2")
+    const [ animated3, setAnimated3 ] = useState("no-animate3")
     return (
         <MyHeader>
             <span className="brand">MR</span>
@@ -35,10 +40,10 @@ export default function Header() {
                     <LI>resume</LI>
                     <LI>contact</LI>
                 </Links>
-                <HamburgerMenu onClick={openMobileMenu}>
-                    <div className="top stripe"></div>
-                    <div className="middle stripe"></div>
-                    <div className="bottom stripe"></div>
+                <HamburgerMenu onClick={toggleMobileMenu}>
+                    <div className={`top stripe ${animated1}`}></div>
+                    <div className={`middle stripe ${animated2}`}></div>
+                    <div className={`bottom stripe ${animated3}`}></div>
                 </HamburgerMenu>
             </nav>
         </MyHeader>
